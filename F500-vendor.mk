791745cf86792386a726d7549fe2c757d3d3a391
lomatik <ralenko2012@gmail.com>
2020-10-05 11:08:38 +0000

    3.18.19 based tree

---
+++ F500-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Ergo/F500/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Ergo/F500/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Ergo/F500/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Ergo/F500/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Ergo/F500/proprietary/vendor,system/vendor)
